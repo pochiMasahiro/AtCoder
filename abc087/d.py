fb24ed8c96d327a4df2ab1aa7ce249575c5dd17f
M.Fukuoka <pochi.masahrio@gmail.com>
2018-01-30 04:08:20 +0900

    BFS

--- abc087/d.py
+++ abc087/d.py
@@ -5,31 +5,34 @@ M = l1[1]
 
 dat = []
 
-per_dis = [[ 0 for n in range(N+1)] for n in range(N+1)]
+#per_dis = [[ 0 for n in range(N+1)] for n in range(N+1)]
+
+per_dis = [ [] for n in range(N+1)]
 
 for n in range(M):
     x = list(map(int, input().split()))
     dat.append(x)
-    
-for x in dat:
-    if per_dis[x[0]][x[1]] == 0 and per_dis[x[0]][x[1]] == 0:
-        per_dis[x[0]][x[1]] = x[2]
-        per_dis[x[1]][x[0]] = -x[2]
-        for i in range(1,N):
-            for j in range(i+1,N):
-                if per_dis[i][j] != 0 and per_dis[i+1][j+1] != 0 and per_dis[i][j+1] == 0:
-                    per_dis[i][j+1] = per_dis[i][j] + per_dis[i+1][j+1]
-                    per_dis[j+1][i] = -per_dis[i][j+1]
-                elif per_dis[i][j] == 0 and per_dis[i+1][j+1] != 0 and per_dis[i][j+1] != 0:
-                    per_dis[i][j] = per_dis[i][j+1] - per_dis[i+1][j+1]
-                    per_dis[j][i] = -per_dis[i][j]
-                elif per_dis[i][j] != 0 and per_dis[i+1][j+1] == 0 and per_dis[i][j+1] != 0:
-                    per_dis[i+1][j+1] = per_dis[i][j+1] - per_dis[i][j]
-                    per_dis[j+1][i+1] = -per_dis[i+1][j+1]
-    else:
-        if abs(per_dis[x[0]][x[1]]) != x[2]:
-            print("No")
-            sys.exit(0)
 
+def bfs(start, goal):
+    visited = []
+    unvisited = [(start, 0)]
+    while unvisited:
+        now = unvisited[0]
+        unvisited = unvisited[1:]
+        for next in per_dis[now[0]]:
+            if next[0] not in visited:
+                if next[0] == goal:
+                    return now[1] + next[1]
+                unvisited.append((next[0], now[1] + next[1]))
+                visited.append(next[0])
+    return None
+
+for x in dat:
+    if None == bfs(x[0], x[1]):
+        per_dis[x[0]].append((x[1],x[2]))
+        per_dis[x[1]].append((x[0],-x[2]))
+    elif  x[2] != bfs(x[0], x[1]):
+        print("No")
+        sys.exit(0)
 
 print("Yes")
