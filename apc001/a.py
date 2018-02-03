c3714c5178535a1e007875cc46fea3805493f55e
M.Fukuoka <pochi.masahrio@gmail.com>
2018-02-04 04:15:29 +0900

    1_005, 1_006 WA

--- apc001/a.py
+++ apc001/a.py
@@ -5,7 +5,4 @@ x, y = list(map(abs, list(map(int, input().split()))))
 if x == y or x % y == 0 or y % x == 0:
     print(-1)
 else:
-    for i in range(2, x if x > y else y):
-        if (i * x) % y != 0:
-            print(i*x)
-            break
+    print(x)
