64ba112ea8e3d2dfdce3b5524eda9199d154f789
M.Fukuoka <pochi.masahrio@gmail.com>
2018-02-03 23:04:04 +0900

    not completed

--- abc087/d.py
+++ abc087/d.py
@@ -3,36 +3,40 @@ l1 = list(map(int, input().split()))
 N = l1[0]
 M = l1[1]
 
-dat = []
-
-#per_dis = [[ 0 for n in range(N+1)] for n in range(N+1)]
 
 per_dis = [ [] for n in range(N+1)]
+people = [None] * (N+1)
 
 for n in range(M):
     x = list(map(int, input().split()))
-    dat.append(x)
+    per_dis[x[0]].append((x[1], x[2]))
+    per_dis[x[1]].append((x[0], -x[2]))
 
-def bfs(start, goal):
+if M != 0:
     visited = []
-    unvisited = [(start, 0)]
+    unvisited = [(x[0], 0)]
+    people[x[0]] = 0
     while unvisited:
         now = unvisited[0]
         unvisited = unvisited[1:]
         for next in per_dis[now[0]]:
             if next[0] not in visited:
-                if next[0] == goal:
-                    return now[1] + next[1]
+                people[next[0]] =  now[1] + next[1]
                 unvisited.append((next[0], now[1] + next[1]))
-                visited.append(next[0])
-    return None
+                visited.append(now[0])
 
-for x in dat:
-    if None == bfs(x[0], x[1]):
-        per_dis[x[0]].append((x[1],x[2]))
-        per_dis[x[1]].append((x[0],-x[2]))
-    elif  x[2] != bfs(x[0], x[1]):
-        print("No")
-        sys.exit(0)
+for i, x in enumerate(per_dis):
+    if x != None:
+        for y in x:
+            if y[1] < 0:
+                if people[y[0]] - people[i] != y[1]:
+                    print("No")
+                    sys.exit()
+            else:
+                if people[i] - people[y[0]] != -y[1]:
+                    print("No")
+                    sys.exit()
+
 
 print("Yes")
+                
