e3ecd12708d09029df3bcbf6d228a443fa85e4f0
M.Fukuoka <pochi.masahrio@gmail.com>
2018-02-05 03:01:53 +0900

    complete B

--- apc001/b.py
+++ apc001/b.py
@@ -1,14 +1,16 @@
 import sys
+import math
 
 N = int(input())
-A = sorted(list(map(int, input().split())))
-B = sorted(list(map(int, input().split())))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
-print(A)
-print(B)
-for x in zip(A,B):
-    if (x[1]-x[0]) < 0:
-        print("No")
-        break
-else:
+d = [ y - x for x, y in zip(A, B)]
+
+q = [ x // 2 for x in d if x > 0]
+r = [ -x for x in d if x < 0]
+    
+if sum(q) >= sum(r):
     print("Yes")
+else:
+    print("No")
