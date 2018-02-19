e42003c66b794360bf868a0e264782f089246575
M.Fukuoka <pochi.masahrio@gmail.com>
2018-02-19 23:07:24 +0900

    ABC088C:AC

--- abc088/c.py
+++ abc088/c.py
@@ -1,3 +1,15 @@
-c = [list(map(int, input().split())) for _ in range(3)]
+import itertools
+import sys
 
 
+def solve(C):
+    for cc, cn in itertools.combinations(C, 2):
+        for c_c, c_n in itertools.combinations(zip(cc, cn), 2):
+            if c_c[0] + c_n[1] != c_c[1] + c_n[0]:
+                return "No"
+    else:
+        return "Yes"
+        
+if __name__ == "__main__":
+    C = [list(map(int, input().split())) for _ in range(3)]
+    print(solve(C))
